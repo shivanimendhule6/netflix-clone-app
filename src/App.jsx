6ce59b0bb533ffef050d87ef61a5b8127526a4a2
shivanimendhule6 <shivanimendhule6@gmail.com>
2025-05-24 00:59:54 +0530

    cc

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs text-center text-4xl font-bold text-blue-600 text-center mt-10">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
